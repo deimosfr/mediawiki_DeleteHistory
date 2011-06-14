724f724d75d2c63ddc32e011397fe8b111106df6
Deimos <deimos@deimos.fr>
2011-06-14 23:15:40 +0200

    Released version 0.3 of DeleteHistory

--- DeleteHistory.php
+++ DeleteHistory.php
@@ -12,13 +12,13 @@ EOT;
 }
  
 $wgExtensionCredits['specialpage'][] = array(
-    'path' => __FILE__,                                                                     // File name for the extension itself, required to getting the revision number from SVN - string, adding in 1.15
-    'name' => "DeleteHistory",                                                              // Name of extension - string
-    'description' => 'deletehistory-desc',                                                  // Description of what the extension does - string
-    'descriptionmsg' => "deletehistory-desc",                                               // Same as above but name of a message, for i18n - string, added in 1.12.0
-    'version' => '0.2',                                                                     // Version number of extension - number or string
-    'author' => "Pierre Mavro",                                                             // The extension author's name - string
-    'url' => "http://www.mediawiki.org/wiki/Extension:SpecialDeleteHistory",                // URL of extension (usually instructions) - string
+	'path' => __FILE__,     																// File name for the extension itself, required to getting the revision number from SVN - string, adding in 1.15
+	'name' => "DeleteHistory",                   											// Name of extension - string
+	'description' => 'deletehistory-desc',                                              	// Description of what the extension does - string
+	'descriptionmsg' => "deletehistory-desc",												// Same as above but name of a message, for i18n - string, added in 1.12.0
+	'version' => '0.3',         															// Version number of extension - number or string
+	'author' => "Pierre Mavro",         													// The extension author's name - string
+	'url' => "http://www.mediawiki.org/wiki/Extension:SpecialDeleteHistory",   				// URL of extension (usually instructions) - string
 );
 
 $dir = dirname(__FILE__) . '/';
--- DeleteHistory_body.php
+++ DeleteHistory_body.php
@@ -1,130 +1,179 @@
 <?php
 class DeleteHistory extends SpecialPage
 {
-    function __construct()
-    {
-        // Need to belong to Administor group
-        parent::__construct( 'DeleteHistory', 'editinterface' );
-        wfLoadExtensionMessages('DeleteHistory');
-    }
+	function __construct()
+	{
+		// Need to belong to Administor group
+		parent::__construct( 'DeleteHistory', 'editinterface' );
+		wfLoadExtensionMessages('DeleteHistory');
+	}
 
-    function execute( $par )
-    {
+	function execute( $par )
+	{
+        // Get database size function
         function get_db_size( )
         {
-            global $wgRequest, $wgDBname;
-            $dbw =& wfGetDB( DB_MASTER );
+    		global $wgRequest, $wgDBname;
+    		$dbw =& wfGetDB( DB_MASTER );
 
-            $dbsize = $dbw->query( "SELECT table_schema '" . $wgDBname . "', sum( data_length + index_length ) / 1024 / 1024 'DB sze in MB' FROM information_schema.TABLES where TABLE_SCHEMA like '" . $wgDBname . "' GROUP BY table_schema ;");
-            while ($row = $dbw->fetchRow($dbsize))
+			$dbsize = $dbw->query( "SELECT table_schema '" . $wgDBname . "', sum( data_length + index_length ) / 1024 / 1024 'DB sze in MB' FROM information_schema.TABLES where TABLE_SCHEMA like '" . $wgDBname . "' GROUP BY table_schema ;");
+			while ($row = $dbw->fetchRow($dbsize))
             {
                 $size = $row[1];
             }
             return $size;
         }
 
-        global $wgRequest, $wgOut, $wgUser, $wgDBname;
+		global $wgRequest, $wgOut, $wgUser, $wgDBname;
 
-        $this->setHeaders();
+		$this->setHeaders();
 
-        // Check if user is Admin
-        if ( !$this->userCanExecute($wgUser) )
-        {
-            $this->displayRestrictionError();
-            return;
-        }
+		// Check if user is Admin
+		if ( !$this->userCanExecute($wgUser) )
+		{
+			$this->displayRestrictionError();
+			return;
+		}
 
-        $dbw =& wfGetDB( DB_MASTER );
+		$dbw =& wfGetDB( DB_MASTER );
  
-        // Get request data from, e.g.
-        $param = $wgRequest->getText('param');
+		// Get request data from, e.g.
+		$param = $wgRequest->getText('param');
 
-        // Localisation messages
-        $check_only = wfMsg('check_only');
-        $del_hist = wfMsg('del_hist');
-        $del_hist_opt = wfMsg('del_hist_opt');
+		// Localisation messages
+		$check_only = wfMsg('check_only');
+		$del_hist = wfMsg('del_hist');
+		$del_hist_opt = wfMsg('del_hist_opt');
 
-        // User choice
+		// User choice
         $wgOut->addWikiText(wfMsg('what_to_do') . ' :');
-        $wgOut->addHTML("<form action=\"$_SERVER[REQUEST_URI]\" method=\"post\">
-            <input type=\"radio\" name=\"choice\" value=\"0\" checked>$check_only<br />
-            <input type=\"radio\" name=\"choice\" value=\"1\">$del_hist<br />
-            <input type=\"radio\" name=\"choice\" value=\"2\">$del_hist_opt (MySQL)<br /><br />
-            <input type=\"submit\" value=\"Validate\">
-            </form>
-        ");
+		$wgOut->addHTML("<form action=\"$_SERVER[REQUEST_URI]\" method=\"post\">
+			<input type=\"radio\" name=\"choice\" value=\"0\" checked>$check_only<br />
+			<input type=\"radio\" name=\"choice\" value=\"1\">$del_hist<br />
+			<input type=\"radio\" name=\"choice\" value=\"2\">$del_hist_opt (MySQL)<br /><br />
+			<input type=\"submit\" value=\"Validate\">
+			</form>
+		");
 
-        // Choosen action choice
+	    // Choosen action choice
         $show_db_size=0;
         $pwd = getcwd();
 
-        if ((isset($_POST['choice'])) and ($_POST['choice'] == "0"))
-        {
-            $out=shell_exec("php $pwd/maintenance/deleteOldRevisions.php");
-        }
-        elseif ((isset($_POST['choice'])) and ($_POST['choice'] == "1"))
-        {
-            $out=shell_exec("php $pwd/maintenance/deleteOldRevisions.php --delete");
-        }
-        elseif ((isset($_POST['choice'])) and ($_POST['choice'] == "2"))
-        {
+	    if ((isset($_POST['choice'])) and ($_POST['choice'] == "0"))
+	    {
+			$out_logs=shell_exec("php $pwd/maintenance/deleteOldRevisions.php");
+	    }
+		elseif ((isset($_POST['choice'])) and ($_POST['choice'] == "1"))
+		{
+			$out_logs=shell_exec("php $pwd/maintenance/deleteOldRevisions.php --delete");
+		}
+		elseif ((isset($_POST['choice'])) and ($_POST['choice'] == "2"))
+		{
             $show_db_size=1;
-            $dbw->immediateBegin();
+    		$dbw->immediateBegin();
 
             // Get actual database size
             $db_size_old = get_db_size( );
 
             // Delete old history
-            $out=shell_exec("php $pwd/maintenance/deleteOldRevisions.php --delete");
-            $out.="</pre>\n<pre>";
+			$out_logs=shell_exec("php $pwd/maintenance/deleteOldRevisions.php --delete");
 
-            // Get all tables
-            $alltables = $dbw->query( "SHOW TABLES" );
-            while ($row = $dbw->fetchRow($alltables))
-            {
+			// Get all tables
+			$alltables = $dbw->query( "SHOW TABLES" );
+
+            // Show optimize status
+            $out_opt="<table border=1 style='border-style: double;'>
+                <tr>
+                    <th>Tables</th>
+                    <th>Status</th>
+                </tr>
+            ";
+
+			while ($row = $dbw->fetchRow($alltables))
+			{
                 // Optimise tables
-                $out .= 'Optimizing table ' . $row[0] . ' : ';
-                $opt_res = $dbw->query("OPTIMIZE TABLE {$row[0]};");
+                $out_opt .= "<tr>\n<td>";
+				$out_opt .= $row[0] . "</td>\n<td>";
+				$opt_res = $dbw->query("OPTIMIZE TABLE {$row[0]};");
 
-                while ($res = $dbw->fetchRow( $opt_res ))
-                {
+				while ($res = $dbw->fetchRow( $opt_res ))
+				{
                     // Set green color on OK and if table is already up to date
                     if (preg_match ("/OK/i", $res[3]))
                     {
-                        $out .= '<span style="font-weight: bold; color: green">' . $res[3] . '</span>';
+					    $out_opt .= '<span style="font-weight: bold; color: green"> ' . $res[3] . '</span>';
                     }
                     elseif (preg_match ("/Table is already up to date/i", $res[3]))
                     {
-                        $out .= '<span style="color: green">' . $res[3] . '</span>';
+                        $out_opt .= '<span style="color: green">' . $res[3] . '</span>';
                     }
                     else
                     {
-                        $out .= $res[3];
+                        $out_opt .= $res[3];
                     }
-                }
-                $out .= '<br />';
-            }
+				}
+                $out_opt .= '</td></tr>';
+			}
+            $out_opt.="</table>";
 
             // Get size after
             $db_size_new = get_db_size( );
-        }
+		}
 
-        // Fix display pre problem
+		// Fix display pre problem
         if ((isset($_POST['choice'])) and ($_POST['choice'] != ""))
         {
-            $wgOut->addHTML("<br /><hr />");
-            $wgOut->addWikiText(wfMsg('result') . ' :');
+			$wgOut->addHTML("<br />");
+            $wgOut->addWikiText('=' . wfMsg('result') . '=');
 
             // Show DB size if ask to optimize
             if ($show_db_size == 1)
             {
-                $wgOut->addHTML(wfMsg('db_size_old') . '<b>' . $db_size_old . " MB</b><br />");
-                $wgOut->addHTML(wfMsg('db_size_new') . '<b>' . $db_size_new . " MB</b><br />");
-                $wgOut->addHTML(wfMsg('db_space_won') . '<b>' . ($db_size_old - $db_size_new) . " MB</b><br />");
+                function kb_or_mb($db_size_old,$db_size_new)
+                {
+                    $spacewon=($db_size_old - $db_size_new);
+                    if ($spacewon < 1)
+                    {
+                        $spacewon = ($spacewon * 1024) . " KB";
+                    }
+                    else
+                    {
+                        $spacewon = $spacewon . " MB";
+                    }
+                    return $spacewon;
+                }
+                // Show result
+                $wgOut->addHTML("<table border=1'>
+                    <tr>
+                        <td></td>
+                        <th>" . wfMsg('size') . "</th>
+                    </tr><tr>
+                        <th>" . wfMsg('db_size_old') . "</th>
+                        <td>$db_size_old MB</td>
+                    </tr><tr>
+                        <th>" . wfMsg('db_size_new') . "</th>
+                        <td>$db_size_new MB</td>
+                    </tr><tr>
+                        <th>" . wfMsg('db_size_old') . "</th>
+                        <td align='center'><b>" . kb_or_mb($db_size_old,$db_size_new) . "</b></td>
+                    </tr></table><br />
+                ");
             }
-            $wgOut->addHTML("<pre>".$out."</pre>");
-        }
-        return true;
-    }
-}
 
+            if ($out_opt)
+            {
+                $wgOut->addWikiText('=' . wfMsg('opt_stat') . '=');
+    		    $wgOut->addHTML($out_opt . '<br />');
+            }
+            if ($out_logs)
+            {
+                if ($_POST['choice'] == "2")
+                {
+                    $wgOut->addWikiText('=' . wfMsg('logs') . '=');
+                }
+    		    $wgOut->addHTML("<pre>".$out_logs."</pre>");
+            }
+		}
+		return true;
+	}
+}
